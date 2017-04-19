c1427d59137e09867e68c21a10c251ca3fb6b5db
haubar <haubarclack@gmail.com>
2017-04-19 22:06:11 +0800

    update env config

--- test_webhook.js
+++ test_webhook.js
@@ -1,4 +1,5 @@
 'use strict'
+require('dotenv').config()
 const line = require('./index')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -13,9 +14,9 @@ app.use(bodyParser.json({
 
 // init with auth
 line.init({
-  accessToken: '5Yx1j1OSvdJzpP9IbZHkh4Lg2rpSTRQe9NkBO8Li5+Ew4v15z7I5WeZ5U8Zb9MU+zarLo5LWgUGe1808RmeHVDTjr+01SdlWUAmpDd6CQpUDyj9U5IxUrLDB5goDIUjn+mPIGRK3TFp+h9noRU6/ywdB04t89/1O/w1cDnyilFU=',
+  accessToken: process.env.accessToken,
   // (Optional) for webhook signature validation
-  channelSecret: '9c81521d45c7e28c7ad7e679725034c7'
+  channelSecret: process.env.channelSecret
 })
 
 /**
