ac7e69b28d469768f6b71d1416e76c27b20dc82d
haubar <haubarclack@gmail.com>
2017-04-20 23:27:30 +0800

    update reply content

--- test_webhook.js
+++ test_webhook.js
@@ -52,7 +52,12 @@ app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
           {
             type: 'text',
             text: '你為何要跟我說'+event.message.text+'我不想聽!!'
+          },
+          {
+            type: 'image',
+            text: '你給我看這個'+event.message.image+'做啥逆!!'
           }
+         
 
         ]
       })
