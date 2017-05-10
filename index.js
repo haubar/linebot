246c5b1af867db249aac62da5f9335d39e53ebb6
haubar <haubarclack@gmail.com>
2017-05-10 18:33:13 +0800

    update linebot

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-'use strict'
-const Service = require('./lib/clients/v2/Service')
-const SignatureValidator = require('./lib/clients/v2/SignatureValidator')
-
-module.exports = {
-
-  init (options) {
-    this.client = new Service(options)
-    this.validator = new SignatureValidator(options)
-  }
-
-}
+module.exports = require('./lib/linebot');
