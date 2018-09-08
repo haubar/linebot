cdedbd40334ffa8b9e43ad9bccb5b4e15e8cded8
haubar <haubarclack@gmail.com>
2018-09-09 07:24:37 +0800

    ADD DEBUG MESSAGE

--- webhook.js
+++ webhook.js
@@ -39,10 +39,15 @@ function getigImage(ig_options, event) {
         //     let item = new Data_ig(origin.node)
         //     ig_image.push(item)
         // }
-        response.graphgl.hashtag.edge_hashtag_to_top_posts.edges.forEach(function(items) {
+        // response.graphgl.hashtag.edge_hashtag_to_top_posts.edges.forEach(function(items) {
         //     let item = new Data_ig(items.node)
         //     ig_image.push(item)
-        })    
+        // })
+        
+        for (items in response.graphgl.hashtag.edge_hashtag_to_top_posts.edges) {
+            let item = new Data_ig(items.node)
+            ig_image.push(item)
+        }
         return event.reply(response.graphql.hashtag.edge_hashtag_to_top_posts.edges.toString())  
         var random_val = [Math.floor(Math.random() * ig_image.length)]
         var url_image_m = ig_image[random_val].thumbnail_src
