6caae66ed984a43db6a4aaffcbb520e228f003e5
haubar <haubarclack@gmail.com>
2018-09-10 00:18:11 +0800

    ADD DEBUG MESSAGE

--- webhook.js
+++ webhook.js
@@ -60,7 +60,7 @@ function getigImage(ig_options, event) {
         //         ig_image.push(item)
         //     }
         // }
-        return event.reply(array_top_posts.length)  
+        return event.reply(array_top_posts.length.toString())  
         return event.reply(response.graphql.hashtag.edge_hashtag_to_top_posts.toString())  
         // return event.reply(response.graphql.hashtag.edge_hashtag_to_top_posts.edges.toString())  
         var random_val = [Math.floor(Math.random() * ig_image.length)]
