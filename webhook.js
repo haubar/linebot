b9cb458d731ac18bbf67cb3173a55b4336918151
haubar <haubarclack@gmail.com>
2018-09-23 03:50:03 +0800

    add console

--- webhook.js
+++ webhook.js
@@ -97,7 +97,7 @@ bot.on('message', function(event) {
                         uri: 'https://www.googleapis.com/youtube/v3/search?'+'key='+process.env.youtubeToken+'&q='+encode_keyword+'&type=video'+'&part=snippet',
                         json: true
                     };
-                    return event.reply('yt_options');
+                    return event.reply(yt_options.uri);
                     var get_youtube_video = getYoutube(yt_options, event);
             } else {
                 switch (event.message.text) {
