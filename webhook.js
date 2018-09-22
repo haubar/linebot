6d56b6c1c24f594009551c830b73ffce4109a130
haubar <haubarclack@gmail.com>
2018-09-23 04:07:37 +0800

    add console

--- webhook.js
+++ webhook.js
@@ -72,7 +72,7 @@ function getYoutube(yt_options, event) {
         var url_image = video[random_val].video_image_url
        
 
-        return event.replay({text: response})
+        return event.replay('55688')
         // return event.reply({
         //     type: 'video',
         //     originalContentUrl: url_video,
