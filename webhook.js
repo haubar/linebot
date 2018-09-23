e0fe1ed0e39c74f86517fb340ff0fc514728c429
haubar <haubarclack@gmail.com>
2018-09-23 20:33:56 +0800

    修改縮圖大小

--- webhook.js
+++ webhook.js
@@ -91,8 +91,8 @@ function getYoutube(yt_options, event) {
                 "baseUrl": url_image+'?', //防呆line網址縮圖
                 "altText": '........',
                 "baseSize": {
-                    "width": 320,
-                    "height": 180
+                    "width": 400,
+                    "height": 200
                 },
                 "actions": [
                     {
