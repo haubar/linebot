8a839e6c9f07d1327f7365b53e87f956936d9e66
haubar <haubarclack@gmail.com>
2018-09-28 05:43:48 +0800

    update function

--- webhook.js
+++ webhook.js
@@ -1,5 +1,5 @@
 const linebot = require('./index.js');
-const data_r18 = require('./lib/dataflit');
+const dataflit = require('./lib/dataflit');
 const rp = require('request-promise');
 
 const bot = linebot({
@@ -153,7 +153,7 @@ bot.on('message', function(event) {
                         } else {
                             pic_number = '00'+pic_number
                         }
-                        var fix_source_code = new data_r18(pic_code.toLowerCase()).filename()+pic_number 
+                        var fix_source_code = new dataflit(pic_code.toLowerCase()).filename()+pic_number 
                         return event.reply(fix_source_code)
                             var dmm_options = {
                                 uri: 'https://pics.dmm.co.jp/digital/video/'+fix_source_code+'/'+fix_source_code+'ps.jpg',
