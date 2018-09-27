507e7eefc7df39e4bd0941c92112c36413643e1e
haubar <haubarclack@gmail.com>
2018-09-28 04:06:41 +0800

    update function

--- webhook.js
+++ webhook.js
@@ -154,7 +154,7 @@ bot.on('message', function(event) {
                             pic_number = '00'+pic_number
                         }
                         var fix_source_code = new data_r18(pic_code.toLowerCase())+pic_number 
-                        return event.reply(fix_source_code)
+                        return event.reply(fix_source_code.data_r18)
                             var dmm_options = {
                                 uri: 'https://pics.dmm.co.jp/digital/video/'+fix_source_code+'/'+fix_source_code+'ps.jpg',
                                 small: 'https://pics.dmm.co.jp/digital/video/'+fix_source_code+'/'+fix_source_code+'ps.jpg',
