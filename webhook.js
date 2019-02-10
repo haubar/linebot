b87daec291e136b5f1b210f2c260a239ec9ef8fc
haubar <haubarclack@gmail.com>
2019-02-10 17:09:53 +0800

    update

--- webhook.js
+++ webhook.js
@@ -175,7 +175,7 @@ bot.on('message', function(event) {
                         event.reply(JSON.stringify(event.source));
                         break;
                     case '給我group':
-                        event.reply(JSON.stringify(event.source.groupId));
+                        event.reply((event.source.groupId));
                         break;    
                     case 'out':
                         event.leaveGroup(event.source.groupId);
