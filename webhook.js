6a9acd14358b08f30837453d48ed33f455f4bc85
haubar <haubarclack@gmail.com>
2019-04-18 15:24:22 +0800

    remove debug point

--- webhook.js
+++ webhook.js
@@ -184,7 +184,6 @@ bot.on('message', function(event) {
                         json: true
                     };
                 let en_area = trans_lang(lang_options)
-                 return event.reply(en_area);
                     var weather_options = {
                         area: en_area,
                         uri: 'https://api.apixu.com/v1/current.json?key='+process.env.weatherKey+'&q='+en_area,
