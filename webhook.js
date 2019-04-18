08b5b52f4ed9d0861ede6a641f9aa0a284a7ad32
haubar <haubarclack@gmail.com>
2019-04-18 16:26:31 +0800

    update reponse

--- webhook.js
+++ webhook.js
@@ -144,10 +144,11 @@ function getWeather(weather_options, event) {
 
 function trans_lang(lang_options) {
     rp(lang_options).then(function(response) { 
-       return event.reply(JSON.stringify(response))
+        var area = reponst.text
+//        return event.reply(JSON.stringify(response))
 //         return 'taipei'
     }).catch(function (err) {
-        return event.reply(JSON.stringify(lang_options))
+//         return event.reply(JSON.stringify(lang_options))
 //         return err
     })
 }
