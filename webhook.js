e186d1e5fda07532f1695a6373870031bbd1df4c
haubar <haubarclack@gmail.com>
2019-04-19 17:14:12 +0800

    update function to anonymous

--- webhook.js
+++ webhook.js
@@ -141,8 +141,8 @@ function getWeather(weather_options, event) {
     })
 }
 
-async function transLang(lang_options, event) {
-    return rp(lang_options).then(function(response) { 
+var transLang = async function(lang_options, event) {
+    return await rp(lang_options).then(async function(response) { 
         return response.text[0]
     }).catch(function (err) {
         return event.reply('歹勢啦~我找不到你的地區')
