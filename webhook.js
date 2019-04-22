71ab8a258ef49bcb77442d10bfa2567bbaaff674
haubar <haubarclack@gmail.com>
2019-04-22 12:05:02 +0800

    add fucntion return

--- webhook.js
+++ webhook.js
@@ -142,8 +142,8 @@ function getWeather(weather_options, event) {
 }
 
 function transLang(lang_options, event) {
-    rp(lang_options).then(function(response) {
-        return response
+   return rp(lang_options).then(function(response) {
+//         return response
         return response.text[0]
     }).catch(function (err) {
         return event.reply('歹勢啦~我找不到你的地區')
