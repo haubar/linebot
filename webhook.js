a5d8d3a16863a027167295031cdb1720b6ed64e0
haubar <haubarclack@gmail.com>
2019-04-30 16:25:53 +0800

    update

--- webhook.js
+++ webhook.js
@@ -142,7 +142,7 @@ function getWeather(weather_options, event) {
 }
 
 function transLang(lang_options, event) {
-   return rp(lang_options).then(function(response) {
+    rp(lang_options).then(function(response) {
         return event.reply(response.text[0])
     }).catch(function (err) {
         return event.reply('歹勢啦~我不曉得你哩工啥米Q口Q')
