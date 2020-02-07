aec45615b30399266ccf50a1a338ec5e0ffcef41
haubar <haubarclack@gmail.com>
2020-02-08 02:07:42 +0800

    add return msg

--- webhook.js
+++ webhook.js
@@ -224,7 +224,7 @@ bot.on('message', function(event) {
             else if (event.message.text.substr(0.3) == '18#') {
                 let keyword = event.message.text.substr(3)
                 
-                console.log('keyword', event.message.text.substr(3);
+                console.log('keyword', event.message.text.substr(3))
                
                 let filter = encodeURI('SEARCH("'+keyword+'", {name})')
                 console.log('filter', filter);
