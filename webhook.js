90b82ba452f734317bfb367e24ba04b3b4d1ffa2
haubar <haubarclack@gmail.com>
2020-08-20 23:32:34 +0800

    update fix

--- webhook.js
+++ webhook.js
@@ -166,8 +166,8 @@ bot.on('message', function(event) {
                 // firedb.ref("getmessage/").push(ig_keyword);
                 var encode_tag = encodeURIComponent(ig_keyword) 
                     var ig_options = {
-                        //uri: 'https://www.instagram.com/explore/tags/'+ encode_tag +'?__a=1',
-                        uri: 'https://www.instagram.com/graphql/query/?query_hash=298b92c8d7cad703f7565aa892ede943&variables={"tag_name":'+ encode_tag +',"first":0},
+                        uri: 'https://www.instagram.com/explore/tags/'+ encode_tag +'?__a=1',
+                        //uri: 'https://www.instagram.com/graphql/query/?query_hash=298b92c8d7cad703f7565aa892ede943&variables={"tag_name":'+ encode_tag +',"first":0},
                         json: true
                     };
                     var get_ig_image = getigImage(ig_options, event);
