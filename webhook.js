7f65e50cb961dff68d1a43a642f34456000bd5bb
haubar <haubarclack@gmail.com>
2020-10-16 22:14:04 +0800

    add debug

--- webhook.js
+++ webhook.js
@@ -59,6 +59,7 @@ function getImage(eat_options, event) {
 
 function getigImage(ig_options, event) {
     rp(ig_options).then(function(response) {
+        console.log(response)
         var ig_image = []
         var array_top_posts = response.graphql.hashtag.edge_hashtag_to_top_posts.edges
         for (let origin of array_top_posts) {
