3cd99afd85ce2935d9ab85cfae11e6cd23276326
haubar <haubarclack@gmail.com>
2022-01-04 14:37:12 +0800

    fix string int

--- webhook.js
+++ webhook.js
@@ -199,7 +199,7 @@ function getick(price) {
     }
     let part = Math.ceil(disc/(num*1000))   
     increase_price = price + (part*num) 
-    let msg = '最少要跳'+part.toString()+'檔,' + increase_price.toString() + '賣出'
+    let msg = '最少要跳'+""+part+""+'檔,' + ""+increase_price+"" + '賣出'
     return msg
 }
 
