e6ce88b9b2490db5228e0a2c54485b3293012f2b
haubar <haubarclack@gmail.com>
2022-01-04 15:11:07 +0800

    優化手續費

--- webhook.js
+++ webhook.js
@@ -172,7 +172,7 @@ function transLang(lang_options, event) {
 
 // 最低手續費、交易費計算
 function getDiscMsg(price, event) {
-    let disc = ((parseFloat(price))*1000*0.2697/100)
+    let disc = getDisc(price)
     let msg = '最低手續費用計算:' + disc
     return event.reply(msg)
 }
