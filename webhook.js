895c9d09a5a5b1c38b300096a3616cad5d86d251
haubar <haubarclack@gmail.com>
2022-01-24 15:24:34 +0800

    add return date

--- webhook.js
+++ webhook.js
@@ -162,7 +162,7 @@ console.log(err)
 function getReport(category, event) {
     let dt = new Date();
     let today = dt.toLocaleDateString('en-GB').split('/').reverse().join('');
-  
+    return event.reply(today)
     let stock_report = {
        uri: 'https://www.twse.com.tw/fund/T86?response=json&date='+today+'&selectType='+category+'&_=1643005796329'
     }
