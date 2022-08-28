48ac1a36ebf6bbfe308c683595fcbe7ccf7ad77a
haubar <haubarclack@gmail.com>
2022-08-28 22:37:10 +0800

    fix

--- webhook.js
+++ webhook.js
@@ -50,8 +50,8 @@ async function findstock(stock) {
             maxRecords: 1,
             view: 'Grid view',
             filterByFormula: filter
-        }).firstPage(function(err, records) {
-            records.forEach(function(record) {
+        }).firstPage(async function(err, records) {
+            await records.forEach(function(record) {
                 stock_id = record.get('no')
                 console.info("id", stock_id)
             })
