e5ef30b52a0f032f59013ea96b096e1dea488270
haubar <haubarclack@gmail.com>
2023-01-10 22:07:52 +0800

    fix show font

--- webhook.js
+++ webhook.js
@@ -267,8 +267,8 @@ async function getStock(stock, event) {
                     let yd = '昨收價:'+info.y
                     let now_buy = '現買價:'+ (info.b).split("_", 1)
                     let now_sell = '現賣價:'+ (info.a).split("_", 1)
-                    let now_sell_amont = '現賣價:'+ (info.f).split("_", 1)
-                    let now_buy_amont = '現買價:'+ (info.g).split("_", 1)
+                    let now_sell_amont = '現賣量:'+ (info.f).split("_", 1)
+                    let now_buy_amont = '現買量:'+ (info.g).split("_", 1)
                     let disc = '最低手續費用計算:'+ (parseFloat((info.b).split("_", 1))*1000*0.2697/100)
                     let tick = getick((info.b).split("_", 1))
                     let msg = name +" \n"+fullname +" \n"+ now_buy +" \n"+ now_sell_amont +" \n"+ now_sell +" \n"+ now_buy_amont +" \n"+ hight +" \n"+ lock +" \n"+low+" \n"+ down +" \n"+now_qty+" \n"+all_qty+" \n"+disc+" \n"+ tick
@@ -289,8 +289,8 @@ async function getStock(stock, event) {
                     let yd = '昨收價:'+info.y
                     let now_buy = '現買價:'+ (info.b).split("_", 1)
                     let now_sell = '現賣價:'+ (info.a).split("_", 1)
-                    let now_sell_amont = '現賣價:'+ (info.f).split("_", 1)
-                    let now_buy_amont = '現買價:'+ (info.g).split("_", 1)
+                    let now_sell_amont = '現賣量:'+ (info.f).split("_", 1)
+                    let now_buy_amont = '現買量:'+ (info.g).split("_", 1)
                     let disc = '最低手續費用計算:'+ (parseFloat((info.b).split("_", 1))*1000*0.2697/100)
                     let tick = getick((info.b).split("_", 1))
                     let msg = name +" \n"+fullname +" \n"+ now_buy +" \n"+ now_sell_amont +" \n"+ now_sell +" \n"+ now_buy_amont +" \n"+ hight +" \n"+ lock +" \n"+low+" \n"+ down +" \n"+now_qty+" \n"+all_qty+" \n"+disc+" \n"+ tick
