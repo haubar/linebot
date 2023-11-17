6fca759f9db38cb1d9008bdadb8b03a1b21baa2c
haubar <haubarclack@gmail.com>
2023-11-17 16:55:31 +0800

    update msg

--- webhook.js
+++ webhook.js
@@ -375,7 +375,7 @@ bot.on('message', function(event) {
         case 'text':
             if(event.message.text == 'test') {
                 let msg = testigImage()
-                event.reply("one test command")
+                event.reply(msg)
             }
             if (event.message.text.substr(0,1) == '#' && event.message.text.substr(0,2) !== '##') {
                 let ig_keyword = event.message.text.substr(1).trim()
