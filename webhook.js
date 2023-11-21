d068509cd72be228dc9b0d1733615ab96ff114b8
haubar <haubarclack@gmail.com>
2023-11-21 13:33:45 +0800

    fix stock api

--- webhook.js
+++ webhook.js
@@ -306,14 +306,12 @@ async function getStock(stock, event) {
 	    })
 	}
 
-    let stock_tse = {
-       uri: 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_'+stock_id+'.tw&json=1&delay=0'
+    let stock_option = {
+       uri: 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_'+stock_id+'.tw|otc_'+stock_id+'.tw&json=1&delay=0'
     }
-    let stock_otc = {
-       uri: 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=otc_'+stock_id+'.tw&json=1&delay=0'
-    }
+
     
-    rp(stock_tse).then(function(response) {
+    rp(stock_option).then(function(response) {
         let res = JSON.parse(response)
         let info = res.msgArray[0]
         if(!!info){
@@ -336,34 +334,7 @@ async function getStock(stock, event) {
             let msg = name +" \n"+fullname +" \n"+now_level +" \n"+ now_buy +" \n"+ now_buy_amont +" \n"+ now_sell +" \n"+ now_sell_amont +" \n"+ hight +" \n"+ lock +" \n"+low+" \n"+ down +" \n"+now_qty+" \n"+all_qty+" \n"+disc+" \n"+ tick
             return event.reply(msg)
         }else{
-            rp(stock_otc).then(function(response) {
-                let res = JSON.parse(response)
-                let info = res.msgArray[0]
-                if(!!info){
-                    let name = info.c+info.n
-                    let fullname = info.c+'名稱:'+info.nf
-                    let hight = '最高價:'+info.h
-                    let lock = '漲停價:'+info.u
-                    let low = '最低價:'+info.l
-                    let down = '跌停價:'+info.w
-                    let now_qty = '當盤成交量:'+info.tv
-                    let all_qty = '累積成交量:'+info.v
-                    let yd = '昨收價:'+info.y
-                    let now_buy = '現買價:'+ (info.b).split("_", 1)
-                    let now_sell = '現賣價:'+ (info.a).split("_", 1)
-                    let now_level = '漲跌:'+ (parseFloat((info.b).split("_", 1)) - parseFloat(info.y))
-                    let now_sell_amont = '現賣量:'+ (info.f).split("_", 1)
-                    let now_buy_amont = '現買量:'+ (info.g).split("_", 1)
-                    let disc = '最低手續費用計算:'+ (parseFloat((info.b).split("_", 1))*1000*0.2697/100)
-                    let tick = getick((info.b).split("_", 1))
-                    let msg = name +" \n"+fullname+" \n"+now_level +" \n"+ now_buy +" \n"+ now_buy_amont +" \n"+ now_sell +" \n"+ now_sell_amont +" \n"+ hight +" \n"+ lock +" \n"+low+" \n"+ down +" \n"+now_qty+" \n"+all_qty+" \n"+disc+" \n"+ tick
-                    return event.reply(msg)
-                } else {
-                    return event.reply('沒有這筆代號資料喲, 咩噗Q口Q')
-                }
-            }).catch(function (err) {
-                return event.reply('沒有這筆代號資料喲, 咩噗Q口Q')
-            })
+            return event.reply('沒有這筆代號資料喲, 咩噗Q口Q')
         }
     }).catch(function (err) {
         return event.reply('沒有這筆代號資料喲, 咩噗Q口Q')        
@@ -664,10 +635,12 @@ bot.on('follow', function(event) {
     event.reply('follow: ' + event.source.userId);
 });
 
+// 取消追隨
 bot.on('unfollow', function(event) {
     event.reply('unfollow: ' + event.source.userId);
 });
 
+//加入群組或聊天室
 bot.on('join', function(event) {
     if (!!event.source.groupId) {
         let joinid = event.source.groupId
@@ -677,6 +650,7 @@ bot.on('join', function(event) {
     event.reply('join: ' + joinid);
 });
 
+//離開群組或聊天室
 bot.on('leave', function(event) {
     if (!!event.source.groupId) {
         let leaveid = event.source.groupId
@@ -686,9 +660,11 @@ bot.on('leave', function(event) {
     event.reply('leave: ' + leaveid);
 });
 
+
 bot.on('postback', function(event) {
     event.reply('postback: ' + event.postback.data);
 });
+
 
 bot.listen('/webhook', process.env.PORT || 3333, () => {
     console.log('Example app listening on port 3333!')
